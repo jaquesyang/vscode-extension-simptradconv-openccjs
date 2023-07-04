f09eecefb66de06a34700f0849af386eab2f1169
Jaques Yang <jaqcy@qq.com>
2023-07-04 14:57:29 +0800

    l18n

--- src/extension.ts
+++ src/extension.ts
@@ -1,32 +1,30 @@
 import * as vscode from 'vscode';
-const OpenCC = require('opencc-js');
+const opencc = require('opencc-js');
 
 
 export function activate(context: vscode.ExtensionContext) {
-	context.subscriptions.push(vscode.commands.registerTextEditorCommand('simptradconv.conv2simp', (editor) => {
-		return editor.edit(builder => {
-			editor.selections.forEach(selection => {
-				const range = new vscode.Range(selection.start, selection.end);
-				const text = editor.document.getText(range) || '';
+	context.subscriptions.push(
+		vscode.commands.registerTextEditorCommand('simptradconv.conv2simp', (editor) => {
+			return convert(editor, 'tw', 'cn');
+		}));
 
-				const converter = OpenCC.Converter({ from: 'tw', to: 'cn' });
-				builder.replace(selection, converter(text));
-			});
+
+	context.subscriptions.push(
+		vscode.commands.registerTextEditorCommand('simptradconv.conv2trad', (editor) => {
+			return convert(editor, 'cn', 'tw');
+		}));
+}
+
+function convert(editor: vscode.TextEditor, from: string, to: string) {
+	return editor.edit(builder => {
+		editor.selections.forEach(selection => {
+			const range = new vscode.Range(selection.start, selection.end);
+			const text = editor.document.getText(range) || '';
+
+			const converter = opencc.Converter({ from: from, to: to });
+			builder.replace(selection, converter(text));
 		});
-	}));
-
-
-	context.subscriptions.push(vscode.commands.registerTextEditorCommand('simptradconv.conv2trad', (editor) => {
-		return editor.edit(builder => {
-			editor.selections.forEach(selection => {
-				const range = new vscode.Range(selection.start, selection.end);
-				const text = editor.document.getText(range) || '';
-
-				const converter = OpenCC.Converter({ from: 'cn', to: 'tw' });
-				builder.replace(selection, converter(text));
-			});
-		});
-	}));
+	});
 }
 
 export function deactivate() { }
